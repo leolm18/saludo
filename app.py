ee1430b63d884976db557c623fa1ffac0115a1a2
leolm18 <44417642+leolm18@users.noreply.github.com>
2018-10-24 17:03:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ app = Flask(__name__)
  
 @app.route("/")
 def hello():
-    return "Soy un master!"
- 
- @app.route("/index")
- def index():
-      return open("index.html").read()
+    return "Soy un master!" 
+@app.route("/index")
+def index():
+    return open("index.html").read()
  
 if __name__ == "__main__":
     app.run()
